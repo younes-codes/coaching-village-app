59765c6895f3b1a5f9969d7dfa4d99ef1e232f6b
Younes Chaoui <younes.chaoui@partnre.com>
2022-03-06 12:39:52 +0100

    set up deployment heroku

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(__dirname + '/dist/coaching-village'));
+app.get('/*', (req, res, next) => {
+  res.sendFile(path.join(__dirname + '/dist/coaching-village/index.html'));
+})
+app.listen(process.env.PORT || 8080);
